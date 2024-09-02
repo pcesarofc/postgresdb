1f196e702203ed19e36439b064ab32ae32288664
Paulo Cesar <p.cesarofc@gmail.com>
2024-09-02 20:36:40 -0300

    Test appkey

--- server.js
+++ server.js
@@ -5,21 +5,25 @@ import { DatabaseNeon } from "./database.js";
 // CREATE THE APP
 const app = express();
 
-
 // CREATE THE DATABASE INSTANCE
 const database = new DatabaseNeon();
 
-
 // CREATE A NEW PROJECT
-app.get('/projects', async (req, res) => {
-    const projects = await database.list()
+app.get("/projects", async (req, res) => {
+  const appkey = req.headers["appkey"];
+  const dbappkey = process.env.APPKEY;
 
-    return res.status(201).send(projects)
-})
+  if (appkey === dbappkey) {
+    const projects = await database.list();
 
+    return res.status(201).send(projects);   
+  }
+
+  return res.status(404).send('NOT AUTHORIZED')
+});
 
 const PORT = process.env.PORT ?? 3000;
-const HOST = '0.0.0.0';
+const HOST = "0.0.0.0";
 
 // START THE APP
 app.listen(PORT, HOST, () => {});
